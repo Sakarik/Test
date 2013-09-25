a3d4f22b9e3fd92c5046263b2e230363fa6b3009
Sakarik <macintosh@kuwinwpa-138-82.kuwin.ku.ac.th>
2013-09-25 12:59:05 +0700

    first summit

---
+++ Github/src/Test.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class Test {
+	public static void main(String []args){
+		System.out.println("your name is?");
+		Scanner scan = new Scanner(System.in);
+		String name = scan.nextLine();
+		System.out.println("Hello "+name);
+		
+	}
+}
